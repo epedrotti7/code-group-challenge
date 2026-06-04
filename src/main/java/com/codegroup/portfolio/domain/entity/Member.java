01c5c95498f8b7ab9cb2e72233afbbb5cab359e0
Eliton Pedrotti <pedrottieliton777@gmail.com>
2026-06-04 18:08:52 -0300

    feat: add domain entities Member and Project

---
+++ src/main/java/com/codegroup/portfolio/domain/entity/Member.java
@@ -0,0 +1,41 @@
+package com.codegroup.portfolio.domain.entity;
+
+import com.codegroup.portfolio.domain.enums.MemberAttribution;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "members")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private MemberAttribution attribution;
+
+    public boolean isFuncionario() {
+        return attribution == MemberAttribution.FUNCIONARIO;
+    }
+}
